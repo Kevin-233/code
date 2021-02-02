2e1771d0fb3640ac833cfd7c32153adc8c57c454
Kevin-233 <zchao1037193594@163.com>
2021-02-02 21:55:09 +0800

    栈

---
+++ test_02_02/test_02_02/test.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MaxSize 10
+
+typedef struct
+{
+   int data[MaxSize];
+   int top;               //Õ»¶¥Ö¸Õë
+
+}SqStack;
+
+void InitStack(SqStack &S)
+{
+  S.top=-1;
+  
+}
+
+bool StackEmpty(SqStack S)  //ÅÐ¶ÏÕ»¿Õ
+{
+  if(S.top==-1)
+	  return true;
+  else
+	  return false;
+}
+
+//½øÕ»
+bool Push(SqStack &S,int x)
+{
+	if(S.top==MaxSize-1)
+		return false;
+	S.top =S.top+1;
+	S.data[S.top]=x;
+	return true;
+}
+
+//³öÕ»
+bool Pop(SqStack &S,int &x)
+{
+	if(S.top==-1)  //Õ»¿Õ£¬±¨´í
+		return false;
+	x=S.data[S.top];
+	S.top=S.top-1;
+	return true;
+}
+
+int main()
+{
+	SqStack S;
+	InitStack(S);
+
+  return 0;
+}
