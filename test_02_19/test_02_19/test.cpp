6df720b39664fddf5b7cf342224937e2c32c9c9b
Kevin-233 <zchao1037193594@163.com>
2021-02-19 20:48:22 +0800

    c++

---
+++ test_02_19/test_02_19/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int *arr=new int [10];
+	for(int i=0;i<10;i++)
+		arr[i]=i+100;
+	for(int i=0;i<10;i++)
+		cout<<arr[i]<<endl;
+
+	delete[]arr;
+
+	system("pause");
+	return 0;
+}
